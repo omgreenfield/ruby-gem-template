27f4c711a2b4812b1afe1e18d2ce99a6c6ae2e26
Matthew Greenfield <mattgreenfield1@gmail.com>
2024-09-12 15:15:44 -0700

    Reorganizes gem into 'Omg' module

---
+++ lib/omg/gem_template.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Omg
+  class RubyProjectTemplate
+  end
+end
--- lib/ruby_project_template.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class RubyProjectTemplate
-  VERSION = '0.1.0'
-end
-
